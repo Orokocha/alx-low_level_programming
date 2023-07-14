558e6b1dd03e838a09ce13e04fc66e5282cea57c
Orokocha <orokocharles01@gmail.com>
2023-07-14 05:16:13 -0700

    The last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,9 +19,4 @@ int main(void)
         else if (n > 0)
         {
                 printf("%d is %n\n", n, "positive");
-        }
-        else
-        {
-                printf("%d is %n\n", n, "zero");
-        }
-        return (0);
+     
