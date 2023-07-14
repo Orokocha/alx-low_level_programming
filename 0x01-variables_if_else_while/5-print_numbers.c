8947c085785295a55efeb379bea06e6fb48efc7f
Orokocha <orokocharles01@gmail.com>
2023-07-14 07:18:43 -0700

    task 5 crr

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Print number from 0 to 9.
+ * main - Prints numbers.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
-	for (i = 0; i < 10; i++)
+	char num;
+
+	for (num = 0; num <= 9; num++)
 	{
-		printf("%d" , i);
+		printf("%d", num);
 	}
 	putchar('\n');
 	return (0);
