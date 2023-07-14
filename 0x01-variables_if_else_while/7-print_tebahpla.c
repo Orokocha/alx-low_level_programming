23047fb1009ee25125ebbf45ab7063a72bbe5f12
Orokocha <orokocharles01@gmail.com>
2023-07-14 07:13:39 -0700

    task corr

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Print number from 0 to 9.
+ * main - Prints the lowercase alphabet in reverse.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	char ch;
-	for (ch = 'z' ; ch >= 'a'; ch--)
-	{
-		putchar(ch);
-	}
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
 	putchar('\n');
+
 	return (0);
 }
