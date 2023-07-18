3c28403484d13c99d4bc84fc85277b607134d9ce
Orokocha <orokocharles01@gmail.com>
2023-07-18 08:54:47 -0700

    Sign crr

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,18 +9,18 @@ int print_sign(int n)
 
 {
 	if (n > 0)
+        {
+        putchar('+');
+        return (1);
+        }
+        else if (n == 0)
+        {
+        _putchar(48);
+        return (0);
+        }
+        else if (n < 0)
 	{
-	putchar(43);
-	return (1);
+	_putchar('_');
 	}
-	else if (n < 0)
-	{
-	_putchar(45);
 	return (-1);
-	}
-	else
-	{
-	_putchar(48);
-	return o;
-	}
 }
