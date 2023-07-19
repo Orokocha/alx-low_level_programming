b08ebbb7d63e8529e76c2e2af349a68ed87465cf
Orokocha <orokocharles01@gmail.com>
2023-07-19 09:15:32 -0700

    tsk2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,16 @@
 #include "main.h"
-#include <unistd.h>
-
 /**
- * print_alphabet - Entry point
- * Return:0
+ * print_alphabet - Prints alphabet in lowercase
+ *
+ * Return: always 0
  */
-
 void print_alphabet(void)
 {
-	char c = 'a';
-	
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
+	int i;
 
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
 	}
 	_putchar('\n');
-
 }
