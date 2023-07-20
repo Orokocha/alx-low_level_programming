cd7a402179154cc812e39f8eeb864ec5c749dd33
Orokocha <orokocharles01@gmail.com>
2023-07-20 05:11:58 -0700

    1-isdigit.crr

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,12 +5,7 @@
  * @x: The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
-int _isdigit.c(int x)
-
+int _isdigit(int c)
 {
-	if (x >= 48 && x <= 57)
-	{
-	return(1);
-	}
-	return(0);
+return (c >= '0' && c <= '9');
 }
